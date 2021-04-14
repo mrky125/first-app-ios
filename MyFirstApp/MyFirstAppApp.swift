b53d5b9291179a585aa877889c476b4c107f1aad
mrky125 <4ex21ia@gmail.com>
2021-04-15 01:07:28 +0900

    First commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Katsuyuki Mori on 2021/04/15.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
